012cc4954b35775e19dcad1276ae565654c87e43
“AshProdigy” <ashley.rodrigues@aeriestechnology.com>
2023-01-18 16:10:46 +0530

    Added endpoint for aeries

--- routes/musician.js
+++ routes/musician.js
@@ -4,13 +4,17 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
+// aeries endpoint
+router.get('/aeries', (req, res) => {
+  res.status('200').send("message: Welcome to aeries music group!");
+});
+
 // healthcheck
 router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
 // cd check
-
 router.get('/cd', (req, res) => {
   res.status('200').send("Continuos deployment: ok!");
 });
