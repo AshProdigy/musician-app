cea923cc3015d9ab2b493c6c66d7f3b9d7cb015e
“AshProdigy” <ashley.rodrigues@aeriestechnology.com>
2023-01-20 12:16:32 +0530

    Added new route for Aeries

--- routes/musician.js
+++ routes/musician.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
+// for aeries
+router.get('/aeries', (req, res) => {
+  res.status('200').send("This route is for Aeries!");
+});
+
 // healthcheck
 router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
